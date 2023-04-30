89423d564757fe90b2e0741dc2f99e71d698229c
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2023-04-30 17:01:05 -0400

    Fix base url config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/favicon.svg',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://www.launchtabs.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/railsconf2023',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
